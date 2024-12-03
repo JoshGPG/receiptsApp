ece5fe070c8a60c8419877170d1b2bc439bd7fbf
Riley Weld <weldrt2820@uwec.edu>
2024-12-03 14:56:53 -0600

    working on fixes
    fixing things

--- ReceiptsAppFile/app/src/main/java/com/example/testapp/MainActivity.java
+++ ReceiptsAppFile/app/src/main/java/com/example/testapp/MainActivity.java
@@ -103,7 +103,7 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-        
+
         categoriesButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
